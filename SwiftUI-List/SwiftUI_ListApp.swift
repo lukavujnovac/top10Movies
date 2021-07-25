d8c6d14d49ff8abd3fd35d21e92ac381a46b861d
Luka <vujnovacluka89@gmail.com>
2021-07-25 14:53:50 +0200

    Initial Commit

---
+++ SwiftUI-List/SwiftUI_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ListApp.swift
+//  SwiftUI-List
+//
+//  Created by Luka Vujnovac on 25.07.2021..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
